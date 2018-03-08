bf508c8a89736153145e36b00d75cdda60fca609
Jason <jasonqiao36@gmail.com>
2018-03-08 14:54:39 +0800

    complete views

--- shortly.py
+++ shortly.py
@@ -7,20 +7,47 @@ from werkzeug.wsgi import SharedDataMiddleware
 from werkzeug.utils import redirect
 from jinja2 import Environment, FileSystemLoader
 try:
-    import urlparse
+    from urlparse import urlparse
 except ImportError:
     from urllib.parse import urlparse
 
 
+def is_valid_url(url):
+    parts = urlparse(url)
+    return parts.scheme in ('http', 'https')
+
+
+def base36_encode(number):
+    assert number >= 0, 'positive integer required'
+    if number == 0:
+        return '0'
+    base36 = []
+    while number != 0:
+        number, i = divmod(number, 36)
+        base36.append('0123456789abcdefghijklmnopqrstuvwxyz'[i])
+    return ''.join(reversed(base36))
+
+
 class Shortly(object):
     def __init__(self, config):
-        self.redis = redis.Redis(config['redis_host'], config['redis_port'])
+        self.redis = redis.Redis(
+            config['redis_host'], config['redis_port'], decode_responses=True)
         template_path = os.path.join(os.path.dirname(__file__), 'templates')
         self.jinja_env = Environment(loader=FileSystemLoader(template_path),
-                                    autoescape=True)
+                                     autoescape=True)
+        self.url_map = Map([
+            Rule('/', endpoint='new_url'),
+            Rule('/<short_id>', endpoint='follow_short_link'),
+            Rule('/<short_id>+', endpoint='short_link_details'),
+        ])
 
     def dispatch_request(self, request):
-        return Response('hello jason')
+        adapter = self.url_map.bind_to_environ(request.environ)
+        try:
+            endpoint, values = adapter.match()
+            return getattr(self, 'on_' + endpoint)(request, **values)
+        except HTTPException as e:
+            return e
 
     def wsgi_app(self, environ, start_response):
         request = Request(environ)
@@ -34,6 +61,46 @@ class Shortly(object):
         t = self.jinja_env.get_template(template_name)
         return Response(t.render(context), mimetype='text/html')
 
+    def insert_url(self, url):
+        short_id = self.redis.get('reverse-url:' + url)
+        if short_id:
+            return short_id
+        url_num = self.redis.incr('last-url-id')
+        short_id = base36_encode(url_num)
+        self.redis.set('url-target:' + short_id, url)
+        self.redis.set('reverse-url:' + url, short_id)
+        return short_id
+
+    def on_new_url(self, request):
+        error = None
+        url = ''
+        if request.method == 'POST':
+            url = request.form['url']
+            if not is_valid_url(url):
+                error = 'Please a valid url'
+            else:
+                short_id = self.insert_url(url)
+                return redirect('{}+'.format(short_id))
+        return self.render_template('new_url.html', error=error, url=url)
+
+    def on_follow_short_link(self, request, short_id):
+        link_target = self.redis.get('url-target:' + short_id)
+        if link_target is None:
+            raise NotFound()
+        self.redis.incr('click-count:' + short_id)
+        return redirect(link_target)
+
+    def on_short_link_details(self, request, short_id):
+        link_target = self.redis.get('url-target:' + short_id)
+        if link_target is None:
+            raise NotFound()
+        click_count = int(self.redis.get('click-count:' + short_id) or 0)
+        return self.render_template('short_link_details.html',
+                                    link_target=link_target,
+                                    short_id=short_id,
+                                    click_count=click_count
+                                    )
+
 
 def create_app(redis_host='localhost', redis_port=6379, with_static=True):
     app = Shortly({
@@ -43,7 +110,7 @@ def create_app(redis_host='localhost', redis_port=6379, with_static=True):
     if with_static:
         app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
             'static': os.path.join(os.path.dirname(__file__), 'static')
-    })
+        })
     return app
 
 
